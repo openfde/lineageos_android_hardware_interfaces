df3aa321fdd2249ad58759663f9e628fe4828532
pngcui <pngcui1224@163.com>
2026-05-29 15:17:01 +0800

    fix tiktok play video abnormality
    use yv12 convert to bgra in mesa

--- graphics/mapper/2.0/utils/passthrough/include/mapper-passthrough/2.0/Gralloc0Hal.h
+++ graphics/mapper/2.0/utils/passthrough/include/mapper-passthrough/2.0/Gralloc0Hal.h
@@ -101,6 +101,14 @@ class Gralloc0HalImpl : public Hal {
         return Error::NONE;
     }
 
+    Error needConvertFormat(native_handle_t* bufferHandle, uint32_t* result) override {
+        if (!mModule->need_convert_format) {
+            return Error::BAD_BUFFER;
+        }
+        *result = mModule->need_convert_format(mModule, bufferHandle);
+        return Error::NONE;
+    }
+
     Error lock(const native_handle_t* bufferHandle, uint64_t cpuUsage,
                const IMapper::Rect& accessRegion, base::unique_fd fenceFd,
                void** outData) override {
