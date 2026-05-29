302943da47b43640251c3eab46bfac6759e07959
pngcui <pngcui1224@163.com>
2026-05-29 15:04:06 +0800

    Revert "support yv12 covert to rgb565 in mesa env"
    This reverts commit 4c43fad75eb336b214238cec24d166e41793df07.

--- graphics/mapper/2.0/utils/hal/include/mapper-hal/2.0/MapperHal.h
+++ graphics/mapper/2.0/utils/hal/include/mapper-hal/2.0/MapperHal.h
@@ -38,8 +38,6 @@ class MapperHal {
     virtual Error importBuffer(const native_handle_t* rawHandle,
                                native_handle_t** outBufferHandle) = 0;
 
-    virtual Error needCovertFormat(native_handle_t* handle, uint32_t* result) = 0;
-
     // free an imported buffer handle
     virtual Error freeBuffer(native_handle_t* bufferHandle) = 0;
 
